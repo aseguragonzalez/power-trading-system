c5ea1463c6984d0316b6cdf5d916381acd8d9f20
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-07 13:40:48 +0000

    chore: report improvements

--- src/TradingSystem.Domain/Report.cs
+++ src/TradingSystem.Domain/Report.cs
@@ -6,32 +6,49 @@ public sealed class Report
 {
     private readonly double[] volumes = new double[24];
 
-    public readonly DateTime Date;
-
     public readonly DateTime CreatedAt;
 
-    public Report(DateTime date, DateTime createdAt)
+    public readonly DateTime Date;
+
+    public readonly int Offset;
+
+    public Report(DateTime createdAt, DateTime date, int offset = 0)
     {
+        if (createdAt.Kind != DateTimeKind.Utc)
+        {
+            throw new ArgumentOutOfRangeException(nameof(createdAt), "CreatedAt must be in UTC.");
+        }
+
+        if (date.Kind != DateTimeKind.Utc)
+        {
+            throw new ArgumentOutOfRangeException(nameof(date), "Date must be in UTC.");
+        }
+
         if (date < createdAt)
         {
             throw new ArgumentOutOfRangeException(nameof(date), "Date cannot be in the past.");
         }
 
-        Date = date;
-        CreatedAt = createdAt;
+        this.Date = date;
+        this.CreatedAt = createdAt;
+        this.Offset = offset;
     }
 
-    public Report(DateTime date) : this(date, createdAt: DateTime.UtcNow) { }
+    public Report(DateTime date, int offset) : this(date: date, createdAt: DateTime.UtcNow, offset: offset) { }
 
-    public IEnumerable<ReportPosition> GetPositions(int timeOffset = 0)
+    public string ReportName => $"PowerPosition_{this.Date:yyyyMMdd}_{this.CreatedAt:yyyyMMddHHmm}.csv";
+
+    public IEnumerable<ReportPosition> GetPositions()
     {
-        DateTime baseLine = new(this.CreatedAt.Year, this.CreatedAt.Month, this.CreatedAt.Day, this.CreatedAt.Hour + timeOffset, 0, 0);
+        DateTime baseLine = new(this.CreatedAt.Year, this.CreatedAt.Month, this.CreatedAt.Day, this.CreatedAt.Hour + this.Offset, 0, 0);
 
         return this.volumes.Select((volume, offset) => new ReportPosition(baseLine.AddHours(offset + 1), volume));
     }
 
     public void AddTradePositions(TradePositions tradePositions)
     {
+        ArgumentNullException.ThrowIfNull(tradePositions);
+
         foreach (TradePosition tradePosition in tradePositions.Positions)
         {
             this.volumes[tradePosition.PeriodId - 1] += tradePosition.Volume;
