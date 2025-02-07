bc3f104af931b304336be1f0461e48ab1a87429e
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-07 19:02:41 +0000

    chore: implement CsvReportRepository

---
+++ src/TradingSystem.Infrastructure/Repositories/CsvReportRepository.cs
@@ -0,0 +1,36 @@
+using System.Globalization;
+using TradingSystem.Domain;
+
+namespace TradingSystem.Infrastructure.Repositories;
+
+public sealed class CsvReportRepository : IReportRepository
+{
+    private readonly CsvReportRepositorySettings settings;
+
+    public CsvReportRepository(CsvReportRepositorySettings settings)
+    {
+        ArgumentNullException.ThrowIfNull(settings, nameof(settings));
+
+        this.settings = settings;
+    }
+
+    public async Task Save(Report report)
+    {
+        ArgumentNullException.ThrowIfNull(report, nameof(report));
+
+        if (!Directory.Exists(settings.Directory))
+        {
+            Directory.CreateDirectory(settings.Directory);
+        }
+
+        using FileStream csvReportStream = File.OpenWrite(Path.Combine(settings.Directory, report.ReportName));
+        await using StreamWriter writer = new(csvReportStream);
+        await writer.WriteLineAsync("Datetime;Volume");
+        foreach (ReportPosition position in report.GetPositions())
+        {
+            string datetime = position.Slot.ToString("yyyy-MM-ddTHH:mm:ssZ", CultureInfo.InvariantCulture);
+            string volume = position.Volume.ToString("F2", CultureInfo.InvariantCulture);
+            await writer.WriteLineAsync($"{datetime};{volume}");
+        }
+    }
+}
