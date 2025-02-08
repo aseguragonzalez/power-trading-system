e877024e796e8d75f9ff45f981d8b376aed09b44
Alfonso Segura <a.segura.gonzalez@gmail.com>
2025-02-08 18:01:35 +0000

    refactor: avoid using magic numbers

--- src/TradingSystem.Domain/Services/TradePosition.cs
+++ src/TradingSystem.Domain/Services/TradePosition.cs
@@ -2,14 +2,17 @@ namespace TradingSystem.Domain.Services;
 
 public sealed class TradePosition
 {
+    private const int FirstPeriodId = 1;
+    private const int LastPeriodId = 24;
+
     public readonly int PeriodId;
 
     public readonly double Volume;
 
     public TradePosition(int periodId, double volume)
     {
-        ArgumentOutOfRangeException.ThrowIfLessThan(periodId, 1, nameof(periodId));
-        ArgumentOutOfRangeException.ThrowIfGreaterThan(periodId, 24, nameof(periodId));
+        ArgumentOutOfRangeException.ThrowIfLessThan(periodId, FirstPeriodId, nameof(periodId));
+        ArgumentOutOfRangeException.ThrowIfGreaterThan(periodId, LastPeriodId, nameof(periodId));
         PeriodId = periodId;
         Volume = volume;
     }
